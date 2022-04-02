070e916a490a285bfbcb94d1404f8b01d5da782f
Markus Luan <markus23_luan@hotmail.com>
2022-04-02 20:03:18 -0300

    Add script para alternar X|O

--- js/engine.js
+++ js/engine.js
@@ -1,7 +1,12 @@
 const game_content = document.getElementById("game_content");
+const quant_players = 2;
+
+let player_atual = 1;
+let jogadas = 0;
 
 function iniciar_jogo(){
     game_content.innerHTML = "";
+    player_atual = 1;
 
     let table = document.createElement("table");
     
@@ -9,16 +14,31 @@ function iniciar_jogo(){
         let tr = document.createElement("tr");
         for (let coluna=0; coluna<3; coluna+=1) {
             let td = document.createElement("td");
-            td.onclick = () => {
-                if (td.innerHTML) return;
-                
-                td.innerHTML = "X";
-                td.style.cursor = "default";
-            };
+            td.onclick = on_grid_click;
             tr.append(td);
         }
         table.append(tr);
     }
 
     game_content.append(table);
+}
+
+function on_grid_click() {
+    if (this.innerHTML) return;
+    
+    if (player_atual == 1) {
+        this.style.color = "#0000FF";
+        this.innerHTML = "X";
+    } else {
+        this.style.color = "#00FF00";
+        this.innerHTML = "O";
+    }
+
+    this.style.cursor = "default";
+
+    if (player_atual < quant_players) {
+        player_atual += 1;
+    }else player_atual = 1;
+
+    jogadas += 1;    
 }
