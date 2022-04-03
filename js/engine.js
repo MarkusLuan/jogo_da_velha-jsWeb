c7b9a0a34f5da81a5fe426f3038dc85bc6e0d101
Markus Luan <markus23_luan@hotmail.com>
2022-04-03 16:29:00 -0300

    Verificando ganhador pela vertical

--- js/engine.js
+++ js/engine.js
@@ -7,6 +7,7 @@ const txt_rodadas = document.getElementById("txt_rodadas");
 const quant_players = 2;
 const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+let matriz;
 let players;
 let player_atual;
 let rodadas = -1;
@@ -41,48 +42,62 @@ function iniciar_jogo(){
     player_atual = 0;
     rodadas += 1;
 
+    // Reiniciar jogadas dos jogadores
     for (let player of players){
         player.jogadas = 0;
     }
     atualizar_titulo_player(players[player_atual]);
     
+    // Reiniciar matriz do jogo
+    matriz = [];
     for (let linha=0; linha<3; linha+=1) {
         let tr = document.createElement("tr");
+
+        let colunas_matriz = [];
         for (let coluna=0; coluna<3; coluna+=1) {
             let td = document.createElement("td");
-            td.onclick = on_grid_click;
+            td.onclick = (e)=> {
+                on_grid_click(e, linha, coluna);
+            };
             tr.append(td);
+
+            colunas_matriz.push(null);
         }
         tbl_game_grid.append(tr);
+
+        matriz.push(colunas_matriz);
     }
 
     txt_rodadas.innerText = `Rodadas: ${rodadas}`;
 }
 
 function is_fim_jogo(){
-    let linhas = tbl_game_grid.getElementsByTagName("tr");
-
     let quant_letras_diagonal_principal = {};
     let quant_letras_diagonal_secundaria = {};
-    let quant_preenchidos = 0;
-    for (let l = 0; l<linhas.length; l+=1){
-        let colunas = linhas[l].getElementsByTagName("td");
+    let totalmente_preenchido = true;
 
+    for (let l=0; l<matriz.length; l+=1){
         let quant_letras_linha = {};
-        for (let c = 0; c<colunas.length; c+=1){
-            let letra = colunas[c].innerText;
+        for (let c=0; c<matriz[l].length; c+=1){
+            let letra = "";
+            if (matriz[l][c]){
+                letra = matriz[l][c].letra;
+            }else {
+                totalmente_preenchido = false;
+                continue;
+            }
 
+            // Verificar letras na diagonal principal
             if (l == c) {
                 quant_letras_diagonal_principal[letra] = (quant_letras_diagonal_principal[letra] + 1) || 1;
             }
 
-            if ((l + c) == (linhas.length-1)){
+            // Verificar letras na diagonal secundaria
+            if ((l + c) == (matriz.length-1)){
                 quant_letras_diagonal_secundaria[letra] = (quant_letras_diagonal_secundaria[letra] + 1) || 1;
             }
 
             quant_letras_linha[letra] = (quant_letras_linha[letra] + 1) || 1;
-
-            if (letra) quant_preenchidos += 1;
 
             // Verificar ganhador
             if (letra){
@@ -92,25 +107,42 @@ function is_fim_jogo(){
                     quant_letras_linha[letra] | 0
                 );
 
-                if (maior_quantidade == colunas.length){
-                    ganhador = players.filter((p) => {
-                        return p.letra == letra;
-                    })[0];
-
-                    ganhador.vitorias += 1;
-
+                if (maior_quantidade == matriz.length){
+                    ganhador = matriz[l][c];
                     return true;
                 }
             }
         }
     }
 
-    return (quant_preenchidos == Math.pow(linhas.length, 2));
+    // Verificar se tem ganhador na vertical
+    for (let c=0; c<matriz.length; c+=1){
+        let ganhou = true;
+        let player = matriz[0][c];
+        if(!player) continue;
+
+        for(let l=0; l<matriz.length; l+=1){
+            if (matriz[l][c] != player) {
+                ganhou = false;
+                break;
+            }
+        }
+
+        if (ganhou) {
+            ganhador = player;
+            return true;
+        }
+    }
+
+    return totalmente_preenchido;
 }
 
 setInterval(() => {
     if (is_fim_jogo()){
-        if (ganhador) alert(`${ganhador.nome} Ganhou!`);
+        if (ganhador) {
+            alert(`${ganhador.nome} Ganhou!`);
+            ganhador.vitorias += 1;
+        }
         else alert("Deu empate!");
 
         ganhador = undefined;
@@ -126,15 +158,18 @@ function change_player(){
     atualizar_titulo_player(players[player_atual]);
 }
 
-function on_grid_click() {
-    if (this.innerHTML) return;
-    this.style.cursor = "default";
+function on_grid_click(event, linha, coluna) {
+    if (matriz[linha][coluna]) return;
+    let grid = event.target;
+
+    grid.style.cursor = "default";
 
     players[player_atual].jogadas += 1;
 
     let player = players[player_atual];
-    this.style.color = player.cor;
-    this.innerHTML = player.letra;
+    grid.style.color = player.cor;
+    grid.innerHTML = player.letra;
+    matriz[linha][coluna] = player;
 
     change_player();
 }
