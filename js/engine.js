675f1419eb4b1f24d90b012c1bf44b1210899669
Markus Luan <markus23_luan@hotmail.com>
2022-04-03 01:10:40 -0300

    Criando class modelo para player

--- js/engine.js
+++ js/engine.js
@@ -1,15 +1,44 @@
 const game_content = document.getElementById("game_content");
+const txt_player = document.getElementById("txt_player");
+const txt_player_plays = document.getElementById("txt_player_plays");
 const tbl_game_grid = document.getElementById("tbl_game_grid");
+const txt_rodadas = document.getElementById("txt_rodadas");
 
 const quant_players = 2;
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let player_atual = 1;
-let jogadas = 0;
+let players;
+let player_atual;
+let rodadas = -1;
+
+function atualizar_titulo_player(player){
+    txt_player.innerHTML = `${player.nome} - <font style=\"color: ${player.cor}\" >${player.letra}</font>`;
+    txt_player_plays.innerText = `Jogadas: ${player.jogadas}`;
+}
+
+function iniciar_players() {
+    players = [];
+    player_atual = 0;
+
+    for (let a=0; a<quant_players; a+=1){
+        let r = parseInt(Math.random() * 255);
+        let g = parseInt(Math.random() * 255);
+        let b = parseInt(Math.random() * 255);
+        let cor = `rgb(${r}, ${g}, ${b})`;
+
+        let letra = letras[parseInt(Math.random() * 26)];
+
+        let player = Player(`Player ${a+1}`, cor, letra);
+        players.push(player);
+    }
+
+    atualizar_titulo_player(players[player_atual]);
+}
 
 function iniciar_jogo(){
     tbl_game_grid.innerHTML = "";
-
-    player_atual = 1;    
+    iniciar_players();
+    rodadas += 1;
     
     for (let linha=0; linha<3; linha+=1) {
         let tr = document.createElement("tr");
@@ -20,24 +49,28 @@ function iniciar_jogo(){
         }
         tbl_game_grid.append(tr);
     }
+
+    txt_rodadas.innerText = `Rodadas: ${rodadas}`;
+}
+
+function change_player(grid){
+    let player = players[player_atual];
+    console.log(player);
+
+    grid.style.color = player.cor;
+    grid.innerHTML = player.letra;
+
+    if ((player_atual+1) < quant_players) {
+        player_atual += 1;
+    }else player_atual = 0;
+    
+    atualizar_titulo_player(players[player_atual]);
 }
 
 function on_grid_click() {
     if (this.innerHTML) return;
-    
-    if (player_atual == 1) {
-        this.style.color = "#0000FF";
-        this.innerHTML = "X";
-    } else {
-        this.style.color = "#00FF00";
-        this.innerHTML = "O";
-    }
-
     this.style.cursor = "default";
 
-    if (player_atual < quant_players) {
-        player_atual += 1;
-    }else player_atual = 1;
-
-    jogadas += 1;    
+    players[player_atual].jogadas += 1;
+    change_player(this);
 }
---
+++ js/models.js
@@ -0,0 +1,8 @@
+const Player = (nome, cor, letra) => {
+    return {
+        nome: nome,
+        cor: cor,
+        letra: letra,
+        jogadas: 0
+    };
+};
